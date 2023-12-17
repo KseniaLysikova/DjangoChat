98918dce6d6c946421035ee27b7377e05b91e785
IlyaCoolDev <ilya@d3en.ru>
2023-12-17 17:16:41 +0300

    fixed problems

--- MyApp/app/settings.py
+++ MyApp/app/settings.py
@@ -86,17 +86,7 @@ CHANNEL_LAYERS = {
     }
 }
 WSGI_APPLICATION = 'app.wsgi.application'
-ASGI_APPLICATION = 'app.asgi.application'
 
-=======
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-        },
-    },
-}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
